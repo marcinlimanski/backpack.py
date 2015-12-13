ee13cc875b025ce6f2c20178423ce538970cd7c4
marcinlimanski <m.limanski@2012.hull.ac.uk>
2015-12-13 20:39:55 +0000

    Added generation of uuid

--- backpack.py
+++ backpack.py
@@ -1,9 +1,11 @@
+import uuid
+
 #Marcin Limanski (13.12.2015) backpack module
 class Storage():
 	empCount = 0
 	"""docstring for Storage"""
 	def __init__(self):
-		Backpack.empCount += 1
+		Storage.empCount += 1
 	#This function will save a given object
 	def putIn(self):
 		return
@@ -23,21 +25,26 @@ class Backpack(Storage):
 		Backpack.empCount += 1
 	#Implementing the inheritend save method 
 	def putIn(self, name):
+		newID = IdGen()
+		id = newID.generateID()
 		print('Save funstion is working ' + name)
+		print(type(id))
 
 	#Implementing the inhereted delete function
-	def takeOut(self, ):
+	def takeOut(self):
 		print('Delete function is working')
 
 	#Implementing the inhereted
-	def lookFor(self, ):
+	def lookFor(self):
 		print('Find function is working')
 
 class IdGen():
+	empCount = 0
 	"""docstring for IdGen"""
-	def __init__(self, name, time):
-		self.name = name
-		self.time = time
-
+	def __init__(self):
+		IdGen.empCount += 1
+	#Retruns a unique id based on the host ID and current time
+	def generateID(self):
+		return uuid.uuid1()
 		
 		
