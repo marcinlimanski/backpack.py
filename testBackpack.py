6705d1ad31e2370d6f8b0c464e37603be7363338
Marcin Limanski <mlimansk@cisco.com>
2015-12-21 13:19:29 +0000

    code clean

--- testBackpack.py
+++ testBackpack.py
@@ -2,17 +2,16 @@
 import backpack
 
 myBackpack = backpack.NewBackpack('myBackpack');
-myBackpack2 = backpack.NewBackpack('myBackpack2');
 
 myBackpack.putIn('marcin', 'dasd')
 myBackpack.putIn('marcin', 'dasd')
 myBackpack.putIn('marcin2', 322)
 
 for i in range (0, 10, 1):
-	myBackpack2.putIn(i, i)
+	myBackpack.putIn(i, i)
 
 print(myBackpack.showAllItems())
-print(myBackpack2.showAllItems())
+
 
 print(id(myBackpack))
-print(id(myBackpack2))
+
